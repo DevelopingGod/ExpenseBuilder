c7792250fabfe8b0b6da06bcc9d936bb1f92163d
DevelopinGod <indishsankalp2004@gmail.com>
2025-12-28 11:11:43 +0530

    Updated the versions in the gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         // FIX 2: Raised minSdk to 26 to support Apache POI (Excel)
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
